a8d270053b482283b3898346ba95556d686bf4ca
pandaQ <fury886@163.com>
2023-11-12 23:08:11 +0900

    output added

--- args_or_kwargs.py
+++ args_or_kwargs.py
@@ -16,3 +16,14 @@ if __name__=="__main__":
 
     # printAll(1,2,3,['a','b','c'],dict)
     printAll(1,2,3,['a','b','c'],**dict)
+
+
+# output
+
+# type:int, value:1
+# type:int, value:2
+# type:int, value:3
+# type:list, value:['a', 'b', 'c']
+# key:name, type:str, value:zhangsan
+# key:age, type:str, value:20
+# key:skill, type:list, value:['eat', 'sleep', 'drink']
